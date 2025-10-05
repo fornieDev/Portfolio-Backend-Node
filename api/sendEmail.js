c8481a74f2899c8f4837d1e47d6583c35d072074
Javier Fornié Ortega <jfornieortega@gmail.com>
2025-10-05 18:08:17 +0200

    CORS añadido a api

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -1,6 +1,15 @@
 import nodemailer from "nodemailer";
 
 export default async function handler(req, res) {
+
+  res.setHeader("Access-Control-Allow-Origin", "https://fornie-dev-portfolio.vercel.app"); // tu frontend
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Método no permitido" });
   }
